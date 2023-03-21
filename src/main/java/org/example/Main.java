e6ebf7c8087171d00ebfc72813749a8c1e6e2e14
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-21 16:21:13 +0200

    First Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,49 @@
 package org.example;
 
+class Person{
+    protected int age;
+
+    public void greet(){
+        System.out.println("Hello");
+    }
+
+    public final void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class Teacher extends Person{
+    public void explain(){
+        System.out.println("Explanation begins");
+    }
+
+}
+
+class Student extends Person{
+    public void goToClasses(){
+        System.out.println("I’m going to class.");
+    }
+
+    public void showAge(){
+        System.out.println("My age is: "+age+" years old");
+    }
+
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person=new Person();
+        person.greet();
+
+        Student student=new Student();
+        student.setAge(21);
+        student.greet();
+        student.showAge();
+
+        Teacher teacher=new Teacher();
+        teacher.setAge(30);
+        teacher.greet();
+        teacher.explain();
     }
 }
